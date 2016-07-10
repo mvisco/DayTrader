a65a3c94bcb5e4cc2d40d24fb9e0d5f3e67f5557
Mario Visco <mariovisco@yahoo.com>
2016-07-10 11:30:23 -0600

    Adding vagrant file

---
+++ Vagrant/shell-provisioner.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y vbox-guest-additions
+sudo apt-get install -y gnome-shell  
+sudo apt-get install -y ubuntu-gnome-desktop
+sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y maven
+sudo apt-get install -y git
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+sudo su - vagrant << EOF
+echo "IN"
+whoami
+bash Anaconda-2.3.0-Linux-x86_64.sh -b /home/vagrant/anaconda
+echo "PATH=$PATH:/home/vagrant/anaconda/bin" >> .bashrc
+sudo apt-get install -y eclipse
+EOF
+echo "OUT"
+whoami
